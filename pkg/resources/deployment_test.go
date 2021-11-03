1e84a62c2fb72419340deedbcde2c5499e42fbb5
Dustin Scott <sdustin@vmware.com>
2021-11-03 14:38:09 -0500

    test: add deployment test
    Signed-off-by: Dustin Scott <sdustin@vmware.com>

---
+++ pkg/resources/deployment_test.go
@@ -0,0 +1,166 @@
+/*
+	SPDX-License-Identifier: MIT
+*/
+
+package resources
+
+import (
+	"testing"
+
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestDeploymentResource_IsReady(t *testing.T) {
+	var randomInt int32
+	randomInt = 1
+
+	type fields struct {
+		parent *appsv1.Deployment
+	}
+	type args struct {
+		resource *resource
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "deploymont should be ready",
+			want:    true,
+			wantErr: false,
+			args: args{
+				resource: &resource{
+					ResourceCommon: ResourceCommon{
+						Name:      "ready",
+						Namespace: "ready",
+					},
+				},
+			},
+			fields: fields{
+				parent: &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:       "ready",
+						Namespace:  "ready",
+						Generation: int64(randomInt),
+					},
+					Spec: appsv1.DeploymentSpec{
+						Replicas: &randomInt,
+					},
+					Status: appsv1.DeploymentStatus{
+						Replicas:           randomInt,
+						ReadyReplicas:      randomInt,
+						ObservedGeneration: int64(randomInt),
+					},
+				},
+			},
+		},
+		{
+			name:    "deploymont should not be ready (replicas)",
+			want:    false,
+			wantErr: false,
+			args: args{
+				resource: &resource{
+					ResourceCommon: ResourceCommon{
+						Name:      "not-ready-replicas",
+						Namespace: "not-ready-replicas",
+					},
+				},
+			},
+			fields: fields{
+				parent: &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:       "not-ready-replicas",
+						Namespace:  "not-ready-replicas",
+						Generation: int64(randomInt),
+					},
+					Spec: appsv1.DeploymentSpec{
+						Replicas: &randomInt,
+					},
+					Status: appsv1.DeploymentStatus{
+						Replicas:           randomInt,
+						ReadyReplicas:      randomInt + 1,
+						ObservedGeneration: int64(randomInt),
+					},
+				},
+			},
+		},
+		{
+			name:    "deploymont should not be ready (name)",
+			want:    false,
+			wantErr: false,
+			args: args{
+				resource: &resource{
+					ResourceCommon: ResourceCommon{
+						Name:      "not-ready-name",
+						Namespace: "not-ready-name",
+					},
+				},
+			},
+			fields: fields{
+				parent: &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace:  "not-ready-name",
+						Generation: int64(randomInt),
+					},
+					Spec: appsv1.DeploymentSpec{
+						Replicas: &randomInt,
+					},
+					Status: appsv1.DeploymentStatus{
+						Replicas:           randomInt,
+						ReadyReplicas:      randomInt,
+						ObservedGeneration: int64(randomInt),
+					},
+				},
+			},
+		},
+		{
+			name:    "deploymont should not be ready (observed generation)",
+			want:    false,
+			wantErr: false,
+			args: args{
+				resource: &resource{
+					ResourceCommon: ResourceCommon{
+						Name:      "not-ready-generation",
+						Namespace: "not-ready-generation",
+					},
+				},
+			},
+			fields: fields{
+				parent: &appsv1.Deployment{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:       "not-ready-generation",
+						Namespace:  "not-ready-generation",
+						Generation: int64(randomInt),
+					},
+					Spec: appsv1.DeploymentSpec{
+						Replicas: &randomInt,
+					},
+					Status: appsv1.DeploymentStatus{
+						Replicas:           randomInt,
+						ReadyReplicas:      randomInt,
+						ObservedGeneration: int64(randomInt + 1),
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			deployment := &DeploymentResource{
+				parent: tt.fields.parent,
+			}
+			got, err := deployment.IsReady(tt.args.resource)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DeploymentResource.IsReady() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("DeploymentResource.IsReady() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
